717ee584ece64652fc38180918a98ecb7bc4e21c
Luis <luis@staudt-fridingen.de>
2023-04-02 21:12:21 +0200

    added read and write of Properties

---
+++ src/main/java/net/luis/data/properties/PropertiesReader.java
@@ -0,0 +1,123 @@
+package net.luis.data.properties;
+
+import com.google.common.collect.Maps;
+import net.luis.data.common.FileHelper;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+public class PropertiesReader implements Closeable {
+	
+	private final Map<String, String> properties;
+	
+	public PropertiesReader(String file) {
+		this(new File(file), '=');
+	}
+	
+	public PropertiesReader(File file) {
+		this(file, '=');
+	}
+	
+	public PropertiesReader(String file, char delimiter) {
+		this(new File(file), delimiter);
+	}
+	
+	public PropertiesReader(File file, char delimiter) {
+		//region Validation
+		if (file == null) {
+			throw new IllegalArgumentException("File cannot be null");
+		}
+		if (file.isDirectory()) {
+			throw new IllegalArgumentException("File cannot be a directory");
+		}
+		if (!file.exists()) {
+			throw new IllegalArgumentException("File does not exist");
+		}
+		if (!file.canRead()) {
+			throw new IllegalArgumentException("File cannot be read");
+		}
+		//endregion
+		this.properties = read(file, delimiter);
+	}
+	
+	private static @NotNull Map<String, String> read(File file, char delimiter) {
+		Map<String, String> properties = Maps.newHashMap();
+		for (String line : FileHelper.readLines(file)) {
+			if (line.startsWith("#")) {
+				continue;
+			}
+			String[] split = line.split(String.valueOf(delimiter));
+			if (split.length == 2) {
+				properties.put(split[0], split[1]);
+			}
+		}
+		return properties;
+	}
+	
+	//region Read properties
+	public Map<String, String> readProperties() {
+		return this.properties;
+	}
+	//endregion
+	
+	//region Read overloads
+	public int readInt(String key) {
+		return Integer.parseInt(this.readProperty(key));
+	}
+	
+	public long readLong(String key) {
+		return Long.parseLong(this.readProperty(key));
+	}
+	
+	public double readDouble(String key) {
+		return Double.parseDouble(this.readProperty(key));
+	}
+	
+	public boolean readBoolean(String key) {
+		return Boolean.parseBoolean(this.readProperty(key));
+	}
+	
+	public String[] readStringArray(String key) {
+		return this.readProperty(key).replace("[", "").replace("]", "").strip().split(",");
+	}
+	
+	public int[] readIntArray(String key) {
+		return Arrays.stream(this.readStringArray(key)).mapToInt(Integer::parseInt).toArray();
+	}
+	
+	public long[] readLongArray(String key) {
+		return Arrays.stream(this.readStringArray(key)).mapToLong(Long::parseLong).toArray();
+	}
+	
+	public double[] readDoubleArray(String key) {
+		return Arrays.stream(this.readStringArray(key)).mapToDouble(Double::parseDouble).toArray();
+	}
+	
+	public boolean[] readBooleanArray(String key) {
+		String[] stringArray = this.readStringArray(key);
+		boolean[] booleanArray = new boolean[stringArray.length];
+		for (int i = 0; i < stringArray.length; i++) {
+			booleanArray[i] = Boolean.parseBoolean(stringArray[i]);
+		}
+		return booleanArray;
+	}
+	//endregion
+	
+	public String readProperty(String key) {
+		return this.properties.get(key);
+	}
+	
+	@Override
+	public void close() throws IOException {
+		this.properties.clear();
+	}
+}
---
+++ src/main/java/net/luis/data/properties/PropertiesWriter.java
@@ -0,0 +1,166 @@
+package net.luis.data.properties;
+
+import com.google.common.collect.Maps;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+public class PropertiesWriter implements Flushable, Closeable {
+	
+	private static final List<String> ALLOWED_EXTENSIONS = List.of(".props", ".properties", ".cfg", ".conf", ".config", ".configuration", ".ini", ".settings", ".prefs", ".preferences");
+	
+	private final Map<String, String> writtenProperties = Maps.newHashMap();
+	private final PropertiesConfig config;
+	private final BufferedWriter writer;
+	
+	public PropertiesWriter(String file) {
+		this(new File(file), PropertiesConfig.DEFAULT);
+	}
+	
+	public PropertiesWriter(File file) {
+		this(file, PropertiesConfig.DEFAULT);
+	}
+	
+	public PropertiesWriter(String file, PropertiesConfig config) {
+		this(new File(file), config);
+	}
+	
+	public PropertiesWriter(File file, PropertiesConfig config) {
+		this.config = config;
+		//region Validation
+		if (file == null) {
+			throw new IllegalArgumentException("File cannot be null");
+		}
+		if (file.isDirectory()) {
+			throw new IllegalArgumentException("File cannot be a directory");
+		}
+		if (!file.exists()) {
+			throw new IllegalArgumentException("File does not exist");
+		}
+		if (!file.canWrite()) {
+			throw new IllegalArgumentException("File cannot be written to");
+		}
+		if (!ALLOWED_EXTENSIONS.contains(FilenameUtils.getExtension(file.getName())) || !this.config.allowCustomExtensions()) {
+			throw new IllegalArgumentException("File extension is not allowed");
+		}
+		//endregion
+		try {
+			this.writer = new BufferedWriter(new FileWriter(file, this.config.allowAppend()));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void writeComment(String comment) {
+		if (this.config.allowComments()) {
+			this.write(comment.startsWith("#") ? comment : "# " + comment);
+		} else {
+			throw new UnsupportedOperationException("Comments are not allowed in this configuration");
+		}
+	}
+	
+	//region Write properties
+	public void writeProperties(@NotNull Map<String, String> properties) throws IOException {
+		properties.forEach(this::writeProperty);
+	}
+	
+	public void writeProperties(@NotNull Properties properties) throws IOException {
+		properties.forEach((key, value) -> this.writeProperty(key.toString(), value.toString()));
+	}
+	//endregion
+	
+	//region Write overloads
+	public void writeProperty(String key, int value) {
+		this.writeProperty(key, String.valueOf(value));
+	}
+	
+	public void writeProperty(String key, long value) {
+		this.writeProperty(key, String.valueOf(value));
+	}
+	
+	public void writeProperty(String key, double value) {
+		this.writeProperty(key, String.valueOf(value));
+	}
+	
+	public void writeProperty(String key, boolean value) {
+		this.writeProperty(key, String.valueOf(value));
+	}
+	
+	public void writeProperty(String key, int[] value) {
+		this.writeProperty(key, Arrays.toString(value));
+	}
+	
+	public void writeProperty(String key, long[] value) {
+		this.writeProperty(key, Arrays.toString(value));
+	}
+	
+	public void writeProperty(String key, double[] value) {
+		this.writeProperty(key, Arrays.toString(value));
+	}
+	
+	public void writeProperty(String key, boolean[] value) {
+		this.writeProperty(key, Arrays.toString(value));
+	}
+	
+	public void writeProperty(String key, String[] value) {
+		this.writeProperty(key, Arrays.toString(value));
+	}
+	//endregion
+	
+	public void writeProperty(String key, String value) {
+		String trimmedKey = StringUtils.trimToEmpty(key);
+		String trimmedValue = StringUtils.trimToEmpty(value);
+		//region Validation
+		if (this.writtenProperties.containsKey(trimmedKey)) {
+			throw new IllegalArgumentException("Property with key " + trimmedKey + " already exists");
+		}
+		if (this.writtenProperties.containsValue(trimmedValue) && !this.config.allowDuplicateValues()) {
+			throw new IllegalArgumentException("Property with value " + trimmedValue + " already exists because the configuration does not allow it");
+		}
+		if (StringUtils.isEmpty(trimmedValue) && !this.config.allowEmptyValues()) {
+			throw new IllegalArgumentException("Property value cannot be empty because the configuration does not allow it");
+		}
+		if (StringUtils.contains(trimmedValue, "\\" + this.config.getDelimiter()) && !this.config.allowEscapedDelimiter()) {
+			throw new IllegalArgumentException("Property value cannot contain escaped delimiter because the configuration does not allow it");
+		}
+		//endregion
+		if (this.config.prettyPrint()) {
+			this.write(trimmedKey + " " + this.config.getDelimiter() + " " + trimmedValue);
+		} else {
+			this.write(trimmedKey + this.config.getDelimiter() + trimmedValue);
+		}
+		this.writtenProperties.put(trimmedKey, trimmedValue);
+	}
+	
+	private void write(String line) {
+		try {
+			this.writer.write(line + System.lineSeparator());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Override
+	public void flush() throws IOException {
+		this.writer.flush();
+	}
+	
+	@Override
+	public void close() throws IOException {
+		this.writtenProperties.clear();
+		this.writer.close();
+	}
+}
