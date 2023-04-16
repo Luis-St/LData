962b315c78bacdd9831b9354e5f8898a2441ca33
Luis <luis@staudt-fridingen.de>
2023-04-16 21:46:21 +0200

    added XmlAttribute and XmlAttributes

---
+++ src/main/java/net/luis/data/xml/attributes/XmlAttribute.java
@@ -0,0 +1,81 @@
+package net.luis.data.xml.attributes;
+
+import net.luis.data.xml.XmlHelper;
+import net.luis.data.xml.config.XmlConfig;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class XmlAttribute {
+	
+	private final String name;
+	private final String value;
+	
+	public XmlAttribute(String name, boolean value) {
+		this(name, String.valueOf(value));
+	}
+	
+	public XmlAttribute(String name, Number value) {
+		this(name, String.valueOf(value));
+	}
+	
+	public XmlAttribute(String name, String value) {
+		this.name = XmlHelper.validateXmlEscape(Objects.requireNonNull(name, "Name must not be null"));
+		this.value = Objects.requireNonNull(value, "Value must not be null");
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public @NotNull XmlAttribute copy() {
+		return new XmlAttribute(this.name, this.value);
+	}
+	
+	//region Getters
+	public String getAsString() {
+		return this.value;
+	}
+	
+	public boolean getAsBoolean() {
+		return Boolean.parseBoolean(this.value);
+	}
+	
+	public int getAsInt() {
+		return Integer.parseInt(this.value);
+	}
+	
+	public long getAsLong() {
+		return Long.parseLong(this.value);
+	}
+	
+	public double getAsDouble() {
+		return Double.parseDouble(this.value);
+	}
+	//endregion
+	
+	public @NotNull String toString(XmlConfig config) {
+		return this.name + "=\"" + this.value + "\"";
+	}
+	
+	//region Object overrides
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof XmlAttribute that)) return false;
+		
+		if (!this.name.equals(that.name)) return false;
+		return this.value.equals(that.value);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.name, this.value);
+	}
+	
+	@Override
+	public String toString() {
+		return "XmlAttribute " + this.name + "=" + this.value;
+	}
+	//endregion
+}
---
+++ src/main/java/net/luis/data/xml/attributes/XmlAttributes.java
@@ -0,0 +1,107 @@
+package net.luis.data.xml.attributes;
+
+import com.google.common.collect.Maps;
+import net.luis.data.xml.config.XmlConfig;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class XmlAttributes implements Iterable<XmlAttribute> {
+	
+	private final Map<String, XmlAttribute> attributes = Maps.newHashMap();
+	
+	public @NotNull XmlAttributes copy() {
+		XmlAttributes attributes = new XmlAttributes();
+		for (Map.Entry<String, XmlAttribute> entry : this.attributes.entrySet()) {
+			attributes.add(entry.getValue().copy());
+		}
+		return attributes;
+	}
+	
+	public boolean has(String name) {
+		return this.attributes.containsKey(name);
+	}
+	
+	public boolean has(XmlAttribute attribute) {
+		return this.attributes.containsValue(attribute);
+	}
+	
+	//region Adders
+	public void add(XmlAttribute attribute) {
+		Objects.requireNonNull(attribute, "Attribute must not be null");
+		this.attributes.put(attribute.getName(), attribute);
+	}
+	
+	public void add(String name, boolean value) {
+		this.add(new XmlAttribute(name, value));
+	}
+	
+	public void add(String name, Number value) {
+		this.add(new XmlAttribute(name, value));
+	}
+	
+	public void add(String name, String value) {
+		this.add(new XmlAttribute(name, value));
+	}
+	//endregion
+	
+	//region Getters
+	public XmlAttribute get(String name) {
+		return this.attributes.get(name);
+	}
+	
+	public List<XmlAttribute> getAttributes() {
+		return List.copyOf(this.attributes.values());
+	}
+	//endregion
+	
+	public int size() {
+		return this.attributes.size();
+	}
+	
+	public boolean isEmpty() {
+		return this.attributes.isEmpty();
+	}
+	
+	public XmlAttribute remove(String name) {
+		return this.attributes.remove(name);
+	}
+	
+	public @NotNull Iterator<XmlAttribute> iterator() {
+		return this.attributes.values().iterator();
+	}
+	
+	public @NotNull String toString(XmlConfig config) {
+		if (this.isEmpty()) {
+			return "";
+		}
+		StringBuilder builder = new StringBuilder();
+		for (XmlAttribute attribute : this) {
+			builder.append(" ").append(attribute.toString(config));
+		}
+		return builder.toString().strip();
+	}
+	
+	//region Object overrides
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof XmlAttributes that)) return false;
+		
+		return this.attributes.equals(that.attributes);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.attributes);
+	}
+	
+	@Override
+	public String toString() {
+		return "XmlAttributes" + this.attributes;
+	}
+	//endregion
+}
