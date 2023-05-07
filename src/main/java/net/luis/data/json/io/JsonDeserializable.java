5a96b0fd2f84eac58c38fbf2f30d1bf743c70313
Luis <luis@staudt-fridingen.de>
2023-05-07 13:41:38 +0200

    setup Deserializable Annotations

--- src/main/java/net/luis/data/json/io/JsonDeserializable.java
+++ src/main/java/net/luis/data/json/io/JsonDeserializable.java
@@ -1,11 +1,20 @@
 package net.luis.data.json.io;
 
+import net.luis.utils.annotation.IndicationInterface;
+
+import java.lang.annotation.*;
+
 /**
  *
  * @author Luis-St
  *
  */
 
+@Inherited
+@Documented
+@IndicationInterface
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.CONSTRUCTOR})
 public @interface JsonDeserializable {
 
 }
